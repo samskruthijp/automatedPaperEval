203ae8391cd5a0b9a555f66597988586fa6ec03a
samskruthijp <3jpsamskruthi@gmail.com>
2020-01-30 18:16:20 +0530

    css loads now

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask,render_template,url_for,request
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-Bootstrap(app)
 
 @app.route("/")
 def home():
-	return "Hello World!"
-if __name__ == '__main__':
+	return render_template('index.html')
+if __name__=='__main__':
 	app.run(debug=True)
+	app.config['TEMPLATES_AUTO_RELOAD'] = True 
+
